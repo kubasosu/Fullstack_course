f9092f2e452cec9b710bb8c8274bf0f3461c893e
sosinskij <ks@4ps.pl>
2023-11-27 12:59:29 +0100

    xd

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,67 @@
-import ReactDOM from 'react-dom/client'
+import React from 'react';
 
-import App from './App'
+// Header component
+const Header = (props) => {
+  console.log(props); // Debugging: Log props to the console
+  return <h1>{props.course}</h1>;
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// Part component
+const Part = ({ part }) => {
+  console.log(part); // Debugging: Log part to the console
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+// Content component
+const Content = ({ parts }) => {
+  console.log(parts); // Debugging: Log parts to the console
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+
+// Total component
+const Total = ({ parts }) => {
+  console.log(parts); // Debugging: Log parts to the console
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+  return (
+    <p><strong>Total exercises: {totalExercises}</strong></p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
+
+  const parts = [part1, part2, part3];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
